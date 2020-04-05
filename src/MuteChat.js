c2fba9588707dcda4a9e1a4e895dd422cc57fea5
Chayapol Moemeng <mchayapol@gmail.com>
2020-04-06 00:12:25 +0700

    Workable version

--- src/MuteChat.js
+++ src/MuteChat.js
@@ -32,10 +32,13 @@ let scoreThreshold = 0.5
 
 //mtcnn options
 let minFaceSize = 20
+let modelLoaded = false
 
 export default function MuteChat() {
     const webcamRef = React.useRef(null);
     const canvasRef = React.useRef(null);
+    const chartRef = React.useRef(null);
+
 
     var data = {
         // labels: ["January", "February", "March", "April", "May", "June", "July"],
@@ -69,19 +72,35 @@ export default function MuteChat() {
             pointHoverBorderWidth: 2,
             pointRadius: 1,
             pointHitRadius: 10,
-            data: [41, 24, 5, 6, 7, 76, 87, 8, 6, 34, 45, , 79, 88, 7, 33, 4, 7, 98]
+            data: [41, 24, 5, 6, 7, 76, 87, 8, 6, 34, 45, 79, 88, 7, 33, 4, 7, 98]
         }]
     };
 
-    const capture = React.useCallback(
-        () => {
-            const imageSrc = webcamRef.current.getScreenshot();
-            console.log(imageSrc);
-        },
-        [webcamRef]
-    );
+    // const capture = React.useCallback(
+    //     () => {
+    //         const imageSrc = webcamRef.current.getScreenshot();
+    //         console.log(imageSrc);
+    //     },
+    //     [webcamRef]
+    // );
 
     useEffect(() => {
+        async function loadModels() {
+            await faceapi.loadSsdMobilenetv1Model('/models')
+            await faceapi.loadFaceLandmarkModel('/models')
+            
+            // await faceapi.loadMtcnnModel('/models')  // Deprecated
+            // await faceapi.loadFaceLandmarkTinyModel('/models')
+            // await faceapi.loadFaceRecognitionModel('/models')
+            // await faceapi.loadFaceExpressionModel('/models')
+            console.log('loadModels')
+
+            const stream = await navigator.mediaDevices.getUserMedia({ video: {} })
+            const videoEl = webcamRef.current
+            videoEl.srcObject = stream
+        }
+
+        loadModels()
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
         ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -100,93 +119,148 @@ export default function MuteChat() {
             )
     }
 
-    const onPlay = (v) => {
-        console.log("Oh yeah")
-        const videoEl = webcamRef.current
-        const options = getFaceDetectorOptions()
-        const result = faceapi.detectSingleFace(videoEl, options).withFaceLandmarks()
-        const canvas = canvasRef.current
-
-        if (result) {
-            // const [x,y] = result.landmarks.positions[1];
-            // console.log('result',x,y)
-            // const p = result.landmarks.positions
-            // console.log('result', p)
-
-            // const landmarkPositions = result.landmarks.positions
-            const landmarks = result.landmarks
-
-            // or get the positions of individual contours,
-            // only available for 68 point face ladnamrks (FaceLandmarks68)
-            const mouth = landmarks.getMouth()
-            const jawOutline = landmarks.getJawOutline()
-            const nose = landmarks.getNose()
-            const leftEye = landmarks.getLeftEye()
-            const rightEye = landmarks.getRightEye()
-            const leftEyeBbrow = landmarks.getLeftEyeBrow()
-            const rightEyeBrow = landmarks.getRightEyeBrow()
-
-            // console.log(mouth.length)
-            // let canvas = $('#overlay').get(0);
-            let ctx = canvas.getContext('2d');
-            // let ctx = document.getElementById('mouthCanvas').getContext('2d');
-            ctx.beginPath();
-            let scale = 3.0
-            let dx = mouth[0].x - 30
-            let dy = mouth[0].y - 30
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-            ctx.strokeStyle = "blue";
-            ctx.fillStyle = '#444';
-
-            // ctx.moveTo((mouth[0].x - dx) * scale, (mouth[0].y - dy) * scale);
-            // mouth.forEach(p => {
-            //   // console.log(p.x, p.y)
-            //   ctx.lineTo((p.x - dx) * scale, (p.y - dy) * scale);
-            // })
-            // ctx.lineTo((mouth[0].x - dx) * scale, (mouth[0].y - dy) * scale);
-
-            // let p1 = mouth[3];
-            // p1.x = (p1.x - dx) * scale
-            // p1.y = (p1.y - dy) * scale
-            ctx.beginPath();
-            mouth.forEach((p, i) => {
-                let px = parseInt((p.x - dx) * scale)
-                let py = parseInt((p.y - dy) * scale)
-                ctx.strokeText(i, px, py);
-                // ctx.arc((p.x - dx) * scale, (p.y - dy) * scale, 5, 0, 2 * Math.PI);
-                ctx.stroke();
-            })
-
-
-            let height = mouth[9].y - mouth[3].y
-            let width = mouth[8].x - mouth[0].x
-
-            data.datasets[0].data.push(width);
-            if (data.datasets[0].data.length > 100)
-                data.datasets[0].data.shift();
-
-            data.datasets[1].data.push(height);
-            if (data.datasets[1].data.length > 100)
-                data.datasets[1].data.shift();
-
-            // chart.update();
+    const getCurrentFaceDetectionNet = function () {
+        if (selectedFaceDetector === SSD_MOBILENETV1) {
+            return faceapi.nets.ssdMobilenetv1
         }
+        if (selectedFaceDetector === TINY_FACE_DETECTOR) {
+            return faceapi.nets.tinyFaceDetector
+        }
+        if (selectedFaceDetector === MTCNN) {
+            return faceapi.nets.mtcnn
+        }
+    }
+
+    const isFaceDetectionModelLoaded = function () {
+        return !!getCurrentFaceDetectionNet().params
+    }
+
+
+    const onPlay = (v) => {
+        async function processFace() {
+            // console.log("Oh yeah")
+            // var videoEl = webcamRef.current.video
+            var videoEl = webcamRef.current
+            // const videoEl = webcamRef.current.getScreenshot()
+            // videoEl = "webcam"
+            // console.log('videoEl', videoEl)
+            // console.log('!isFaceDetectionModelLoaded()', !isFaceDetectionModelLoaded())
+            if (videoEl.paused || videoEl.ended || !isFaceDetectionModelLoaded()) {
+                // if (videoEl.paused || videoEl.ended || !modelLoaded) {
+                // console.log(`onPlay pre-condition failed.
+                //         \tselectedFaceDetector ${selectedFaceDetector}
+                //         \faceapi.nets.ssdMobilenetv1 ${faceapi.nets.ssdMobilenetv1}
+                //         \tvideoEl.paused ${videoEl.paused}
+                //         \tvideoEl.ended ${videoEl.ended}
+                //         \t!isFaceDetectionModelLoaded() ${!isFaceDetectionModelLoaded()}`)
+                return setTimeout(() => onPlay())
+            }
+
+            const options = getFaceDetectorOptions()
+            // console.log('options', options)
+            const result = await faceapi.detectSingleFace(videoEl, options).withFaceLandmarks()
+            // console.log('result', result)
+
+            if (result && result.landmarks) {
+                const canvas = canvasRef.current
+
+                // const [x,y] = result.landmarks.positions[1];
+                // console.log('result',x,y)
+                // const p = result.landmarks.positions
+                // console.log('result', p)
+
+                // const landmarkPositions = result.landmarks.positions
+                const landmarks = result.landmarks
+                // console.log('landmarks', landmarks)
+
+                // or get the positions of individual contours,
+                // only available for 68 point face ladnamrks (FaceLandmarks68)
+                const mouth = landmarks.getMouth()
+                // const jawOutline = landmarks.getJawOutline()
+                // const nose = landmarks.getNose()
+                // const leftEye = landmarks.getLeftEye()
+                // const rightEye = landmarks.getRightEye()
+                // const leftEyeBbrow = landmarks.getLeftEyeBrow()
+                // const rightEyeBrow = landmarks.getRightEyeBrow()
+
+                // console.log(mouth.length)
+                // let canvas = $('#overlay').get(0);
+                let ctx = canvas.getContext('2d');
+                // let ctx = document.getElementById('mouthCanvas').getContext('2d');
+                ctx.beginPath();
+                let scale = 3.0
+                let dx = mouth[0].x - 30
+                let dy = mouth[0].y - 30
+                ctx.fillRect(0, 0, canvas.width, canvas.height);
+                ctx.strokeStyle = "blue";
+                ctx.fillStyle = '#444';
+
+                // ctx.moveTo((mouth[0].x - dx) * scale, (mouth[0].y - dy) * scale);
+                // mouth.forEach(p => {
+                //   // console.log(p.x, p.y)
+                //   ctx.lineTo((p.x - dx) * scale, (p.y - dy) * scale);
+                // })
+                // ctx.lineTo((mouth[0].x - dx) * scale, (mouth[0].y - dy) * scale);
+
+                // let p1 = mouth[3];
+                // p1.x = (p1.x - dx) * scale
+                // p1.y = (p1.y - dy) * scale
+                ctx.beginPath();
+                mouth.forEach((p, i) => {
+                    let px = parseInt((p.x - dx) * scale)
+                    let py = parseInt((p.y - dy) * scale)
+                    ctx.strokeText(i, px, py);
+                    // ctx.arc((p.x - dx) * scale, (p.y - dy) * scale, 5, 0, 2 * Math.PI);
+                    ctx.stroke();
+                })
+
+
+                let height = mouth[9].y - mouth[3].y
+                let width = mouth[8].x - mouth[0].x
+
+                data.datasets[0].data.push(width);
+                if (data.datasets[0].data.length > 100)
+                    data.datasets[0].data.shift();
+
+                data.datasets[1].data.push(height);
+                if (data.datasets[1].data.length > 100)
+                    data.datasets[1].data.shift();
+
+                // let chart = chartRef.current.chart
+                // chart.update()
+
+            }
+
+        }
+        processFace();
+        setTimeout(() => onPlay())
     }
 
     return (
         <Container fluid>
             <Row>
                 <Col rowSpan={2}>
-                    <Webcam
+                    {/* <Webcam
+                        id="webcam"
                         audio={false}
                         // height={720}
                         // width={1280}
                         ref={webcamRef}
+                        onUserMedia={onPlay}
                         screenshotFormat="image/jpeg"
                         videoConstraints={videoConstraints}
-                    />
+                    /> */}
+                    <video
+                        ref={webcamRef}
+                        onLoadedMetadata={onPlay}
+                        id="inputVideo"
+                        // src={navigator.mediaDevices.getUserMedia({ video: {} })}
+                        autoPlay
+                        muted
+                        playsInline
+                        width={400} />
                     <br />
-                    <Button variant="outline-dark" onClick={capture}>Capture photo</Button>
+                    {/* <Button variant="outline-dark" onClick={capture}>Capture photo</Button> */}
 
                 </Col>
                 <Col>
@@ -195,7 +269,11 @@ export default function MuteChat() {
             </Row>
             <Row>
                 <Col>
-                    <LineChart width={800} height={200} />
+                    <LineChart
+                        // ref={chartRef} 
+                        width={800}
+                        height={200}
+                        data={data} />
                 </Col>
             </Row>
         </Container>
